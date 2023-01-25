6938964874972c7f126bcf626f4b451ab781d403
Raoni Andrade <raonio.andrade@gmail.com>
2023-01-25 16:12:08 -0300

    Opening PR

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// OPENING PR
 
 function App() {
   return (
