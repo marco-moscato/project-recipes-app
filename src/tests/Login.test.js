e43870ac879c79e549bee06ffe165d129620b4d7
Raoni Andrade <raonio.andrade@gmail.com>
2023-01-25 18:29:07 -0300

    Testes feitos e corrigidos

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -9,12 +9,17 @@ describe('Testa a tela de Login', () => {
   // renderWithRouter(<App />);
   // });
 
+  const emailId = 'email-input';
+  const loginId = 'login-submit-btn';
+  const passwordId = 'password-input';
+  const emailTest = 'teste@trybe.com';
+
   it('Renderiza os elementos na tela', () => {
     renderWithRouter(<App />);
 
-    const emailInput = screen.getByRole('textbox');
-    const passwordInput = screen.getByPlaceholderText(/password/i);
-    const loginBtn = screen.getByTestId('login-submit-btn');
+    const emailInput = screen.getByTestId(emailId);
+    const loginBtn = screen.getByTestId(loginId);
+    const passwordInput = screen.getByTestId(passwordId);
 
     expect(emailInput).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
@@ -23,22 +28,22 @@ describe('Testa a tela de Login', () => {
   it('Botão habilita e desabilita corretamente', () => {
     renderWithRouter(<App />);
 
-    const emailInput = screen.getByTestId('email-input');
-    const passwordInput = screen.getByPlaceholderText(/password/i);
-    const loginBtn = screen.getByTestId('login-submit-btn');
+    const emailInput = screen.getByTestId(emailId);
+    const loginBtn = screen.getByTestId(loginId);
+    const passwordInput = screen.getByTestId(passwordId);
 
-    userEvent.type(emailInput, 'teste@trybe.com');
+    userEvent.type(emailInput, emailTest);
     userEvent.type(passwordInput, '1234567');
     expect(loginBtn).toBeEnabled();
   });
   it('Botão redireciona para a rota /meals', () => {
     const { history } = renderWithRouter(<App />);
 
-    const emailInput = screen.getByTestId('email-input');
-    const passwordInput = screen.getByPlaceholderText(/password/i);
-    const loginBtn = screen.getByTestId('login-submit-btn');
+    const emailInput = screen.getByTestId(emailId);
+    const loginBtn = screen.getByTestId(loginId);
+    const passwordInput = screen.getByTestId(passwordId);
 
-    userEvent.type(emailInput, 'teste@trybe.com');
+    userEvent.type(emailInput, emailTest);
     userEvent.type(passwordInput, '1234567');
     expect(loginBtn).toBeEnabled();
     userEvent.click(loginBtn);
@@ -48,16 +53,16 @@ describe('Testa a tela de Login', () => {
   it('Verifica se salva email no localStorage', () => {
     renderWithRouter(<App />);
 
-    const emailInput = screen.getByTestId('email-input');
-    const passwordInput = screen.getByPlaceholderText(/password/i);
-    const loginBtn = screen.getByTestId('login-submit-btn');
+    const emailInput = screen.getByTestId(emailId);
+    const loginBtn = screen.getByTestId(loginId);
+    const passwordInput = screen.getByTestId(passwordId);
 
-    userEvent.type(emailInput, 'teste@trybe.com');
+    userEvent.type(emailInput, emailTest);
     userEvent.type(passwordInput, '1234567');
     expect(loginBtn).toBeEnabled();
     userEvent.click(loginBtn);
 
-    const userObj = { email: 'teste@trybe.com' };
+    const userObj = { email: emailTest };
     expect(userObj).toEqual(JSON.parse(localStorage.getItem('user')));
   });
 });
