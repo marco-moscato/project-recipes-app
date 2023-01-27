365f045ee0edc4855eb0cec1d747d289a9330934
Marco Moscato <marcomoscatto@gmail.com>
2023-01-27 19:26:12 -0300

    feat: parte do filtro por categoria (req 20).

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -2,15 +2,17 @@ import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import RecipesContext from '../context/RecipesContext';
 import fetchRecipes from '../services/RecipesApi';
+import FilterByCategory from './FilterByCategory';
 
 function Recipes() {
   const { data, setData } = useContext(RecipesContext);
   const [path, setPath] = useState('');
   const [strName, setStrName] = useState('');
   const [strThumb, setStrThumb] = useState('');
+  const [url, setUrl] = useState('');
+  const [urlCategory, setUrlCategory] = useState('');
   const { location } = useHistory();
   const magic = 12;
-  const [url, setUrl] = useState('');
 
   const renderRecipes = () => {
     if (location.pathname === '/meals') {
@@ -18,18 +20,18 @@ function Recipes() {
       setStrName('strMeal');
       setStrThumb('strMealThumb');
       setUrl('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+      setUrlCategory('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
     }
     if (location.pathname === '/drinks') {
       setPath('drinks');
       setStrName('strDrink');
       setStrThumb('strDrinkThumb');
       setUrl('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+      setUrlCategory('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
     }
   };
   const test = async () => {
     if (!data) setData(await fetchRecipes(url));
-    console.log(data);
-    console.log(await fetchRecipes(url));
   };
 
   useEffect(() => {
@@ -39,6 +41,7 @@ function Recipes() {
 
   return (
     <>
+      <FilterByCategory value={ urlCategory } />
       Recipes
       { data && data[path].map((recipe, index) => (
         index < magic
@@ -58,3 +61,7 @@ function Recipes() {
 }
 
 export default Recipes;
+
+// * Cada botão deve conter o atributo prefixado `data-testid=${categoryName}-category-filter` e devem ser exibidas apenas as 5 primeiras categorias retornadas da API.
+//   * Caso as receitas sejam de comida, deve-se exibir as 5 primeiras categorias de comida obtidas por meio do endpoint `https://www.themealdb.com/api/json/v1/1/list.php?c=list`;
+//   * Caso as receitas sejam de bebida, deve-se exibir as 5 primeiras categorias de bebida obtidas por meio do endpoint `https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list`.
