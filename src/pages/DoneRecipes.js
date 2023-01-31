f3e4eff133b9bedc7239bc7693859cd6ccab8339
Marco Moscato <marcomoscatto@gmail.com>
2023-01-31 16:20:13 -0300

    feat: requisito 50 - elementos da tela de receita favoritas. Foi necessário fazer o requisito 44.

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -8,6 +8,16 @@ function DoneRecipes() {
         title="Done Recipes"
         haveSearch={ false }
       />
+      <button data-testid="filter-by-all-btn">All</button>
+      <button data-testid="filter-by-meal-btn">Meals</button>
+      <button data-testid="filter-by-drink-btn">Drinks</button>
+      <img data-testid={ `${index}-horizontal-image` } alt="" />
+      Imagem do Card
+      <p data-testid={ `${index}-horizontal-top-text` }>Categoria da receita</p>
+      <p data-testid={ `${index}-horizontal-name` }>Nome da receita</p>
+      <p data-testid={ `${index}-horizontal-done-date` }>Data da receita</p>
+      <p data-testid={ `${index}-horizontal-share-btn` }>Compartilhar Receita</p>
+      <p data-testid={ `${index}-${tagName}-horizontal-tag` }>Tag</p>
     </div>
   );
 }
--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import DoneRecipes from './DoneRecipes';
 
 function FavoriteRecipes() {
   return (
@@ -9,9 +10,20 @@ function FavoriteRecipes() {
         title="Favorite Recipes"
         haveSearch={ false }
       />
+      <DoneRecipes />
       <Footer />
     </div>
   );
 }
 
 export default FavoriteRecipes;
+
+// [{
+//   id: id-da-receita,
+//   type: meal-ou-drink,
+//   nationality: nacionalidade-da-receita-ou-texto-vazio,
+//   category: categoria-da-receita-ou-texto-vazio,
+//   alcoholicOrNot: alcoholic-ou-non-alcoholic-ou-texto-vazio,
+//   name: nome-da-receita,
+//   image: imagem-da-receita
+// }]
