0f9aa01b4947689b1d5e040de83cb75bf770a7ef
Raoni Andrade <raonio.andrade@gmail.com>
2023-02-01 17:38:00 -0300

    Salve as receitas favoritas no localStorage

--- src/components/FavoriteBtn.js
+++ src/components/FavoriteBtn.js
@@ -1,48 +1,69 @@
 import { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import blackHeart from '../images/blackHeartIcon.svg';
 import whiteHeart from '../images/whiteHeartIcon.svg';
-import RecipesContext from '../context/RecipesContext';
+// import RecipesContext from '../context/RecipesContext';
 
 function FavoriteBtn(props) {
   const [click, setClick] = useState(true);
-  const { path } = useContext(RecipesContext);
+  // const { path } = useContext(RecipesContext);
   const { value } = props;
+  const { location } = useHistory();
 
-  console.log(path);
+  // console.log(path);
   const arr = [];
 
   const checkPath = () => {
     let favoriteRecipe = {};
-    if (path === 'meals') {
+    if (location.pathname.includes('meals')) {
       favoriteRecipe = {
-        id: value[path][0].idMeal,
+        id: value.meals[0].idMeal,
         type: 'meal',
-        nationality: value[path][0].strArea,
-        category: value[path][0].strCategory,
+        nationality: value.meals[0].strArea,
+        category: value.meals[0].strCategory,
         alcoholicOrNot: '',
-        name: value[path][0].strMeal,
-        image: value[path][0].strMealThumb,
+        name: value.meals[0].strMeal,
+        image: value.meals[0].strMealThumb,
       };
+      // return arr.push(favoriteRecipe);
     }
-    if (path === 'drinks') {
+    if (location.pathname.includes('drinks')) {
       favoriteRecipe = {
-        id: value[path][0].idDrink,
+        id: value.drinks[0].idDrink,
         type: 'drink',
-        nationality: value[path][0].strArea,
-        category: value[path][0].strCategory,
-        alcoholicOrNot: value[path][0].strAlcoholic,
-        name: value[path][0].strDrink,
-        image: value[path][0].strDrinkThumb,
+        nationality: '',
+        category: value.drinks[0].strCategory,
+        alcoholicOrNot: value.drinks[0].strAlcoholic,
+        name: value.drinks[0].strDrink,
+        image: value.drinks[0].strDrinkThumb,
       };
+      // return arr.push(favoriteRecipe);
     }
     return arr.push(favoriteRecipe);
+  };
+
+  // para adicionar e remover FAVORITOS do local storage.
+  const setLocalStorage = (param) => {
+    localStorage.setItem('favoriteRecipes', JSON.stringify(param));
+  };
+  const getLocalStorage = () => JSON.parse(localStorage.getItem('favoriteRecipes'));
+  // const removeFromLocalStorage = () => localStorage.removeItem('favoriteRecipes');
+  const setOrGetLocalStorage = (param) => {
+    if (getLocalStorage() !== null) {
+      const getSaved = [...getLocalStorage(), param];
+      setLocalStorage(getSaved);
+    } else {
+      localStorage.setItem('favoriteRecipes', JSON.stringify(param));
+      console.log(param);
+      console.log('entrou no else do setOrGet');
+    }
   };
 
   const handleClick = () => {
     setClick(!click);
     checkPath();
-    localStorage.setItem('favoriteRecipes', JSON.stringify(arr));
+    setOrGetLocalStorage(arr);
   };
   return (
     <button
